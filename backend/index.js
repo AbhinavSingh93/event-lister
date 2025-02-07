d79f52788b3d8d9c4df036a67932aa49ce1efa92
*AbhinavSingh93* <*abhinavsingh18813@gmail.com*>
2025-02-07 19:00:38 +0530

    deploy!!

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static("front-end/public"));
+app.use(express.static("../front-end/public"));
 
 // Import Routes
 const eventRoutes = require("./routes/events");
